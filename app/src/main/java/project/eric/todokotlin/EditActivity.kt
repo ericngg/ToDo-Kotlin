fc9e4da6b45e0cee4e96f4f7cf3a20b9c1ab8133
Eric Ng <EricNg109@gmail.com>
2022-02-07 12:26:44 -0800

    polished and added comments

--- app/src/main/java/project/eric/todokotlin/EditActivity.kt
+++ app/src/main/java/project/eric/todokotlin/EditActivity.kt
@@ -1,11 +1,36 @@
 package project.eric.todokotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class EditActivity : AppCompatActivity() {
+
+    lateinit var etTask: EditText
+    lateinit var btnSave: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+
+        etTask = findViewById(R.id.etTask)
+        btnSave = findViewById(R.id.btnSave)
+
+        // Initialize the text field
+        etTask.setText(intent?.getStringExtra("task"))
+
+        // btnSave click listener
+        btnSave.setOnClickListener {
+            val data = Intent()
+            data.putExtra("task", etTask.text.toString())
+            data.putExtra("position", intent?.getIntExtra("position", 0))
+
+            setResult(RESULT_OK, data)
+
+            finish()
+        }
+
     }
 }
--- app/src/main/java/project/eric/todokotlin/MainActivity.kt
+++ app/src/main/java/project/eric/todokotlin/MainActivity.kt
@@ -14,15 +14,24 @@ import java.nio.charset.Charset
 
 class MainActivity : AppCompatActivity() {
 
+    private val REQUEST_CODE = 400
+
+    lateinit var etAddTaskField: EditText
+    lateinit var recyclerView: RecyclerView
+    lateinit var btnAdd: Button
+
     var listOfTasks = mutableListOf<String>()
-    lateinit var adapter : TaskItemAdapter
+    lateinit var adapter: TaskItemAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val etAddTaskField = findViewById<EditText>(R.id.etAddTaskField)
+        etAddTaskField = findViewById(R.id.etAddTaskField)
+        recyclerView = findViewById(R.id.rvRecycleView)
+        btnAdd = findViewById(R.id.btnAdd)
 
+        // Long click listener
         val onLongClickListener = object : TaskItemAdapter.OnLongClickListener {
             override fun onItemLongClicked(position: Int) {
                 listOfTasks.removeAt(position)
@@ -30,42 +39,59 @@ class MainActivity : AppCompatActivity() {
 
                 saveItems()
             }
-
         }
 
+        // Click listener
         val onClickListener = object : TaskItemAdapter.OnClickListener {
             override fun onItemClicked(position: Int) {
-                val intent = Intent(applicationContext, EditActivity::class.java)
-
-                startActivity(intent)
+                val intent = Intent(this@MainActivity, EditActivity::class.java)
+                intent.putExtra("task", listOfTasks[position])
+                intent.putExtra("position", position)
+                startActivityForResult(intent, REQUEST_CODE)
             }
-
         }
 
+        // Load items from start
         loadItems()
 
-        val recyclerView = findViewById<RecyclerView>(R.id.rvRecycleView)
-
+        // Adapter initialization
         adapter = TaskItemAdapter(listOfTasks, onLongClickListener, onClickListener)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        findViewById<Button>(R.id.btnAdd).setOnClickListener {
+        // btnAdd click listener
+        btnAdd.setOnClickListener {
             val userInputtedTask = etAddTaskField.text.toString()
             listOfTasks.add(userInputtedTask)
-
             adapter.notifyItemInserted(listOfTasks.size - 1)
-
             etAddTaskField.setText("")
-
             saveItems()
         }
     }
 
+    // Editing functionality
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            var pos = data?.getIntExtra("position", 0)
+            var task = data?.getStringExtra("task")
+
+            if (task != null) {
+                listOfTasks[pos!!] = task
+            }
+
+            saveItems()
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    // Get file function
     fun getDataFile() : File {
         return File(filesDir, "data.txt")
     }
 
+    // Load file function
     fun loadItems() {
         try {
             listOfTasks = FileUtils.readLines(getDataFile(), Charset.defaultCharset())
@@ -74,6 +100,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // Save file function
     fun saveItems() {
         try {
             FileUtils.writeLines(getDataFile(), listOfTasks)
